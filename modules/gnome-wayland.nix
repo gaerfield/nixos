6baf79f01e410fc7bf9782b2c71907afc18bbb66
blatobi <tobias.blaschke@payfree.io>
2024-03-14 13:33:58 +0100

    fix wayland is enabled per default

--- modules/gnome-wayland.nix
+++ modules/gnome-wayland.nix
@@ -2,7 +2,7 @@
 
 {
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.wayland = true;
+  services.xserver.enable = true;
   services.xserver.displayManager.gdm.enable = true;
   services.xserver.desktopManager.gnome.enable = true;
 }
