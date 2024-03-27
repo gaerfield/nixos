c5458f7bd653f340a16dc04ff7798c790acc3ade
gaerfield <gaerfield@users.noreply.github.com>
2024-03-27 11:54:53 +0100

    fix nix-ld and java default installation

--- home/java.nix
+++ home/java.nix
@@ -1,7 +1,4 @@
 { pkgs, ... }: {
-  home.packages = [ pkgs.temurin-bin pkgs.zlib ];
-  home.sessionVariables = {
-    _JAVA_OPTIONS="-Dawt.useSystemAAFontSettings=lcd";
-    LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [ pkgs.zlib ]}:$LD_LIBRARY_PATH";
-  };
+  # zlib is somehow required by java
+  home.packages = [ pkgs.temurin-bin ];
 }
--- modules/nix-ld.nix
+++ modules/nix-ld.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }: {
   programs.nix-ld = {
     enable = true;
-    programs.nix-ld.libraries = with pkgs; [
+    libraries = with pkgs; [
       curl
       expat
       fontconfig
