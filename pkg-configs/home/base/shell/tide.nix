bb872a6392f6fe9d24eb481643e8e6ce86b79633
gaerfield <gaerfield@users.noreply.github.com>
2024-04-09 08:43:29 +0200

    adapt tide theme

--- pkg-configs/home/base/shell/tide.nix
+++ pkg-configs/home/base/shell/tide.nix
@@ -3,7 +3,7 @@
     enable = true;
     # tide does not officially support declarative config
     # this abbr is my replacement for resetting the theme (and avoiding starship)
-    shellAbbrs.tideThemeReset = "tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time=No --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Dark --prompt_spacing=Compact --icons='Few icons' --transient=Yes";
+    shellAbbrs.tideThemeReset = "tide configure --auto --style=Rainbow --prompt_colors='True color' --show_time=No --rainbow_prompt_separators=Angled --powerline_prompt_heads=Sharp --powerline_prompt_tails=Flat --powerline_prompt_style='Two lines, frame' --prompt_connection=Disconnected --powerline_right_prompt_frame=No --prompt_connection_andor_frame_color=Dark --prompt_spacing=Compact --icons='Few icons' --transient=Yes"
     # branchvincent/tide-show-on-cmd
     plugins = with pkgs.fishPlugins; [
       { name = "tide"; src = tide.src; }
