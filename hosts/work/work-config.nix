7dc55ecc9114a4cfe69fba2f9712705822676400
gaerfield <gaerfield@users.noreply.github.com>
2024-04-17 12:08:24 +0200

    add abbreviation

--- hosts/work/work-config.nix
+++ hosts/work/work-config.nix
@@ -1,7 +1,11 @@
 {
   programs.fish = {
     shellAbbrs.bft-gui = {
-      expansion = "xdg-open http://localhost:5200/iot-debug-tool/ && ssh -NT -L 5200:localhost:5200 -L 5100:localhost:5100 -L 42069:localhost:42069 bft-%";
+      expansion = "xdg-open http://localhost:5200/iot-debug-tool/ && ssh -NT -L 5200:localhost:5200 -L 5100:localhost:5100 bft-%";
+      setCursor = true;
+    };
+    shellAbbrs.bft-display = {
+      expansion = "xdg-open http://localhost:5200/checkout-display/ && ssh -NT -L 8080:localhost:8080 -L 80:localhost:80 bft-%";
       setCursor = true;
     };
   };
